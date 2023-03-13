41903bd36dc1b3b639eecf0a8cb683c4f252012e
stassius <35260274+stassius@users.noreply.github.com>
2023-03-13 20:58:17 +0300

    Initial commit

---
+++ MetaParserForms/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿
+namespace MetaParserForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Property = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Value = new System.Windows.Forms.DataGridViewTextBoxColumn();
+
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
+            this.dataGridView1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ColumnHeadersVisible = false;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Property,
+            this.Value});
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.WindowFrame;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.ControlLight;
+            dataGridViewCellStyle1.Padding = new System.Windows.Forms.Padding(0, 10, 0, 20);
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(1348, 827);
+            this.dataGridView1.TabIndex = 1;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Property
+            // 
+            this.Property.HeaderText = "Property";
+            this.Property.Name = "Property";
+            this.Property.ReadOnly = true;
+            this.Property.Width = 180;
+            // 
+            // Value
+            // 
+            this.Value.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.Value.HeaderText = "Value";
+            this.Value.MinimumWidth = 200;
+            this.Value.Name = "Value";
+            this.Value.ReadOnly = true;
+            this.Value.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(1348, 827);
+            this.Controls.Add(this.dataGridView1);
+            this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "MetaParser";
+            this.ShowIcon = false;
+            this.Text = "Image Meta Parser";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Property;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Value;
+    }
+}
+
---
+++ MetaParserForms/Form1.cs
@@ -0,0 +1,169 @@
+﻿using MetadataExtractor;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tulpep.NotificationWindow;
+
+namespace MetaParserForms
+{
+    public partial class Form1 : Form
+    {
+        protected static string CurrentFile;
+        public Form1()
+        {
+            InitializeComponent();
+            TopMost = true;
+            KeyPreview = true;
+            dataGridView1.Columns[1].DefaultCellStyle.WrapMode = DataGridViewTriState.True;
+            dataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            dataGridView1.ShowCellToolTips = false;
+            this.AllowDrop = true;
+            this.DragEnter += new DragEventHandler(Form1_DragEnter);
+            this.DragDrop += new DragEventHandler(Form1_DragDrop);
+            this.KeyDown += new KeyEventHandler(Form1_KeyDown);
+
+            string[] args = Environment.GetCommandLineArgs();
+            if (args.Length > 1)
+            {
+                string filename = args[1];
+                if (File.Exists(filename) == true)
+                {
+                    CurrentFile = filename;
+                    Parse();
+                }
+            }
+        }
+
+        void Form1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop)) e.Effect = DragDropEffects.Copy;
+        }
+
+        void Form1_DragDrop(object sender, DragEventArgs e)
+        {
+            string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+            CurrentFile = files[0];
+            Parse();
+        }
+
+        private void Parse()
+        {
+            if (File.Exists(CurrentFile) == false)
+            {
+                Console.WriteLine("File does not exist!");
+                return;
+            }
+            dataGridView1.Rows.Clear();
+            Image image = new Bitmap(CurrentFile);
+            PropertyItem[] propItems = image.PropertyItems;
+
+            var directories = ImageMetadataReader.ReadMetadata(CurrentFile);
+
+            var tag = directories[1].Tags.FirstOrDefault(x => x.Name == "Textual Data");
+            string prompt, negPrompt;
+            if (tag.Description.Contains("Negative prompt:"))
+            {
+                 prompt = GetInbetweenString(tag.Description, "parameters:", "Negative prompt:").Trim();
+                 negPrompt = GetInbetweenString(tag.Description, "Negative prompt:", "Steps:").Trim();
+            }
+            else
+            {
+                 prompt = GetInbetweenString(tag.Description.ToLower(), "parameters:", "Steps:").Trim();
+                 negPrompt = string.Empty ;
+            }
+            dataGridView1.Rows.Add("Prompt", prompt);
+            dataGridView1.Rows.Add("Neg prompt", negPrompt);
+
+            string rest = tag.Description.Substring(tag.Description.IndexOf("Steps:"));
+            foreach(string unit in rest.Split(","))
+            {
+                dataGridView1.Rows.Add(unit.Split(":")[0].Trim(), unit.Split(":")[1].Trim());
+            }
+        }
+
+        private string GetInbetweenString(string str, string first, string last)
+        {
+            int pFrom = str.IndexOf(first) + first.Length;
+            int pTo = str.LastIndexOf(last);
+            if (pTo - pFrom <= 0) return string.Empty;
+            return str.Substring(pFrom, pTo - pFrom);
+        }
+     
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+                CurrentFile = openFileDialog1.FileName;
+                Parse();
+            }
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            string result = dataGridView1.Rows[e.RowIndex].Cells[1].Value.ToString();
+            Clipboard.SetText(result);
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                Application.Exit();
+            }
+        }
+
+
+          
+        private bool CurrentlyFlashing = false;
+        private async void FlashControl(DataGridViewRow control)
+        {
+            
+            Color flashColor = Color.Green;
+
+            float duration = 100; // milliseconds
+            int steps = 2;
+            float interval = duration / steps;
+
+            if (CurrentlyFlashing) return;
+            CurrentlyFlashing = true;
+            Color original = control.DefaultCellStyle.BackColor;
+
+            float interpolant = 0.0f;
+            while (interpolant < 1.0f)
+            {
+                control.Selected = !control.Selected;
+                //Color c = InterpolateColour(flashColor, original, interpolant);
+                //control.DefaultCellStyle.BackColor = c;
+                await Task.Delay((int)interval);
+                interpolant += (1.0f / steps);
+            }
+
+           // control.DefaultCellStyle.BackColor = original;
+
+            CurrentlyFlashing = false;
+        }
+
+        public static Color InterpolateColour(Color c1, Color c2, float alpha)
+        {
+            float oneMinusAlpha = 1.0f - alpha;
+            float a = oneMinusAlpha * (float)c1.A + alpha * (float)c2.A;
+            float r = oneMinusAlpha * (float)c1.R + alpha * (float)c2.R;
+            float g = oneMinusAlpha * (float)c1.G + alpha * (float)c2.G;
+            float b = oneMinusAlpha * (float)c1.B + alpha * (float)c2.B;
+            return Color.FromArgb((int)a, (int)r, (int)g, (int)b);
+        }
+    }
+}
+
